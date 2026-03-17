4ca4c3d8b57601cd987c6d568a6f55d85d0257f8
root <root@ip-172-31-22-92.ap-south-1.compute.internal>
2026-03-17 18:41:45 +0000

    test 4

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "test 2 o devops webhook  CI/CD pipeline!"
+    return "niya gay"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
